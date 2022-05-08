e4ff2a74fb1070f36565fd9c2b3fd5100e7f192b
Luan <82720293+luandao1012@users.noreply.github.com>
2022-05-08 11:27:47 +0700

    .

--- src/main/java/baith2/Controller/ProductController.java
+++ src/main/java/baith2/Controller/ProductController.java
@@ -5,7 +5,9 @@ import baith2.repository.ProducRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 @Controller
@@ -20,6 +22,39 @@ public class ProductController {
     public String getProduct(Model model) {
         List<Product> productList = producRepository.findAll();
         model.addAttribute("listProduct", productList);
+        return "product";
+    }
+    @GetMapping("/edit/{id}")
+    public String showEdit(@PathVariable(name = "id") String code, Model model){
+        Product product= producRepository.findByCode(code);
+        model.addAttribute("productEdit", product);
+        return "edit";
+    }
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateProduct(@ModelAttribute("productEdit") Product product){
+        producRepository.save(product);
+        return "redirect:/products";
+    }
+    @GetMapping("/addProduct")
+    public String showAddProduct(Model model){
+        model.addAttribute("newProduct", new Product());
+        return "addproduct";
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addProduct(@ModelAttribute("newProduct") Product product){
+        producRepository.save(product);
+        return "redirect:/products";
+    }
+    @GetMapping("/delete/{id}")
+    public String findProduct(Model model, @PathVariable("id") String code) {
+        Product product = producRepository.findByCode(code);
+        model.addAttribute("product", product);
+        return "delete";
+    }
+
+    @RequestMapping(value = "/delete/{code}", method = RequestMethod.POST)
+    public String deleteProduct(@PathVariable("code") String code) {
+        producRepository.deleteByCode(code);
         return "product";
     }
 }
--- src/main/java/baith2/repository/ProducRepository.java
+++ src/main/java/baith2/repository/ProducRepository.java
@@ -3,4 +3,6 @@ package baith2.repository;
 import baith2.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProducRepository extends JpaRepository<Product,Integer> {
+    Product findByCode(String code);
+    Product deleteByCode(String code);
 }
